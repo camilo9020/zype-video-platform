7d8b87f1ed95db85588b66763f2f404a5522d1cc
Juan Camilo Hdz <chernandez@toolboxtve.com>
2021-06-17 22:38:36 -0500

    Add video cards to home using helpers to conditionate the way to aceess videos

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,17 @@
 module HomeHelper
+    def get_video_link(video)
+        if video.video_type == 'premium'
+            user_signed_in? ? video.link : new_user_session_path
+        else
+            video.link
+        end
+    end
+
+    def allow_reproduce(video)
+        if video.video_type == 'premium'
+            user_signed_in? ? true : false
+        else
+            true
+        end
+    end
 end
