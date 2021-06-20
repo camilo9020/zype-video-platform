39e6531283e298f4bb7a1eec72aac1f94949f1ab
Juan Camilo Hdz <chernandez@toolboxtve.com>
2021-06-20 10:31:43 -0500

    Remove devise user model and subscriptions model

--- app/models/subscription.rb
+++
@@ -1,7 +0,0 @@
-class Subscription < ApplicationRecord
-  belongs_to :user_id
-
-  extend Enumerize
-
-  enumerize :status, in: [:inactive, :free_trial, :active]
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_011926) do
+ActiveRecord::Schema.define(version: 2021_06_18_005902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "subscriptions", force: :cascade do |t|
-    t.string "status"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_subscriptions_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
 
   create_table "videos", force: :cascade do |t|
     t.string "title"
@@ -43,5 +23,4 @@ ActiveRecord::Schema.define(version: 2021_06_18_011926) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "subscriptions", "users"
 end
